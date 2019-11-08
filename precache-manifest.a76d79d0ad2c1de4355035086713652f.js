3af74e3335ebb72545b45015c364e5450a96b8c8
Varun <28918411+VarunDevPro@users.noreply.github.com>
2019-11-08 13:29:55 +0530

    Updates

---
+++ precache-manifest.a76d79d0ad2c1de4355035086713652f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a897606e6a17c1bb9f7d10c6e8edc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1425bad81a01bb5d44ce",
+    "url": "/static/css/main.c310a5a4.chunk.css"
+  },
+  {
+    "revision": "5cda7ab1ac9b60314baa",
+    "url": "/static/js/2.86179fb5.chunk.js"
+  },
+  {
+    "revision": "1425bad81a01bb5d44ce",
+    "url": "/static/js/main.0b3ef445.chunk.js"
+  },
+  {
+    "revision": "bec8957a70ce5cf93113",
+    "url": "/static/js/runtime-main.6c78c497.js"
+  }
+]);
