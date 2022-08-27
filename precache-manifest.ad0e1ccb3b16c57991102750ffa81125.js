9e198750be30c70a9151151f73424b09853be054
Varun A <28918411+VarunDevPro@users.noreply.github.com>
2022-08-27 07:25:47 +0000

    Updates

---
+++ precache-manifest.ad0e1ccb3b16c57991102750ffa81125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b452dbc99d157510a30d9114a50b0aaf",
+    "url": "http://localhost:3000/index.html"
+  },
+  {
+    "revision": "f875bd963fb1173a7e81",
+    "url": "http://localhost:3000/static/css/main.56ad5b47.chunk.css"
+  },
+  {
+    "revision": "1d4ebd7316bf53857279",
+    "url": "http://localhost:3000/static/js/2.cec7c51a.chunk.js"
+  },
+  {
+    "revision": "f875bd963fb1173a7e81",
+    "url": "http://localhost:3000/static/js/main.573f32a0.chunk.js"
+  },
+  {
+    "revision": "836b9a6de1405db8dca3",
+    "url": "http://localhost:3000/static/js/runtime-main.19f90825.js"
+  }
+]);
